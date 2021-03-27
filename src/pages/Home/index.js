ea699967b869939c0971f322be3ce2aaf51d3884
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-03-28 01:12:37 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 export default function Home() {
+
+  useEffect(() => {
+    document.title = "Motel Room"
+  })
 
   return (
     <div>
